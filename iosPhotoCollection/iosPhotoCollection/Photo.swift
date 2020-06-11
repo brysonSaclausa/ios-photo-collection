7fa244c2c03f02f8142abb060b5a2effb300842c
Brys1Saclaus2 <brys1saclaus2@gmail.com>
2020-06-11 15:51:31 -0700

    iosPhotoCollection started. -CreatedModel

---
+++ iosPhotoCollection/iosPhotoCollection/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  iosPhotoCollection
+//
+//  Created by B$hady on 6/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+}
---
+++ iosPhotoCollection/iosPhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosPhotoCollection
+//
+//  Created by B$hady on 6/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
